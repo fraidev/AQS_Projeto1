dc14c88a87175f20ba1613b8bee9bca9f00b484a
Felipe Cardozo <felipe.cardozo@sapiensia.com>
2019-02-17 02:11:05 -0300

    works!

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,6 +1,13 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.TypedQuery;
+
 import domain.Bairro;
 import domain.Cidade;
 import domain.Empresa;
@@ -13,64 +20,94 @@ import infra.JPAUtil;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Teste 1");
+		System.out.println("Init");
 
 		EntityManager em = JPAUtil.getEntityManager();
 		
-		String mesTermino = "2011/08";
-		String cnpjEstabelecimento = "Sem Informação";
-		String empresaNome = "CONFECÇÃO A";
-		String rua = "RUA IMIGRANTES, 525";
-		String cepEstabelecimento = "SI";
-		String bairroEstabelecimento = "JARDIM IPIRANGA";
-		String municipio = "Americana";
-		String estado = "São Paulo";
-		
-		Uf uf = new Uf();
-		uf.setSigla(estado);
-		uf.setNome("SP");
-		
-		Cidade cidade = new Cidade();
-		cidade.setNome(municipio);
-		cidade.setUf(uf);
-		
-		Bairro bairro = new Bairro();
-		bairro.setNome(bairroEstabelecimento);
-		bairro.setCidade(cidade);
-		
-		Empresa empresa = new Empresa();
-		empresa.setCnpj(cnpjEstabelecimento);
-		empresa.setRazaoSocial(empresaNome);
-		empresa.setLogadouro(rua);
-		empresa.setCep(cepEstabelecimento);
-		empresa.setBairro(bairro);
-		empresa.setCidade(cidade);
-		empresa.setUf(uf);
-		
-		Fiscalizacao fiscalizacao = new Fiscalizacao();
-		fiscalizacao.setData(mesTermino);
-		fiscalizacao.setEmpresa(empresa);
-		
-		try {
-			em.getTransaction().begin();
+		String csvFile = "C:/Empresas - São Paulo.csv";
+    	String line = "";
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
 
-			em.persist(uf);
-			em.persist(cidade);
-			em.persist(bairro);
-			em.persist(empresa);
-			em.persist(fiscalizacao);
-			
-			
-			
-			em.getTransaction().commit();
-			
-		} catch (Exception e) {
-			em.getTransaction().rollback();
-		} finally {
-			em.close();
-		}
-		
-		System.out.println("Teste 2");
+            while ((line = br.readLine()) != null) {
+
+                // use comma as separator
+                String[] excelLine = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+                
+        		String mesTermino = excelLine[1];
+        		String cnpjEstabelecimento = excelLine[2];
+        		String empresaNome = excelLine[3];
+        		String rua = excelLine[4];
+        		String cepEstabelecimento = excelLine[5];
+        		String bairroEstabelecimento = excelLine[6];
+        		String municipio = excelLine[7];
+        		String estado = excelLine[8];
+        		
+        		//cep valid
+                String regex = "\\d{5}-\\d{3}";
+        		if(cepEstabelecimento.matches(regex)) {
+        			Uf uf = new Uf();
+        			uf.setSigla("SP");
+        			uf.setNome(estado);
+
+    				String jpqlUf = "select u from Uf u where u.sigla = :pNome";
+    				TypedQuery<Uf> queryUf = em.createQuery(jpqlUf, Uf.class);
+    				queryUf.setParameter("pNome", uf.getSigla());
+    				try {
+    					uf = queryUf.getSingleResult();
+    				} catch (NoResultException ex) {
+    					System.out.println(ex);
+    				}
+        			
+        			Cidade cidade = new Cidade();
+        			cidade.setNome(municipio);
+        			cidade.setUf(uf);
+
+//    				String jpql = " select u from Cidade u where u.nome = :pNome";
+//    				TypedQuery<Cidade> query = em.createQuery(jpql, Cidade.class);
+//    				query.setParameter("pNome", cidade.getNome().trim().toLowerCase());
+//    				try {
+//    					cidade = query.getSingleResult();
+//    				} catch (NoResultException ex) {
+//    				}
+        			
+        			Bairro bairro = new Bairro();
+        			bairro.setNome(bairroEstabelecimento);
+        			bairro.setCidade(cidade);
+        			
+        			Empresa empresa = new Empresa();
+        			empresa.setCnpj(cnpjEstabelecimento);
+        			empresa.setRazaoSocial(empresaNome);
+        			empresa.setLogadouro(rua);
+        			empresa.setCep(cepEstabelecimento);
+        			empresa.setBairro(bairro);
+        			empresa.setCidade(cidade);
+        			empresa.setUf(uf);
+        			
+        			Fiscalizacao fiscalizacao = new Fiscalizacao();
+        			fiscalizacao.setData(mesTermino);
+        			fiscalizacao.setEmpresa(empresa);
+            		
+            		try {
+            			em.getTransaction().begin();
+            			em.persist(uf);
+            			em.persist(cidade);
+            			em.persist(bairro);
+            			em.persist(empresa);
+            			em.persist(fiscalizacao);
+            			em.getTransaction().commit();
+            			
+            		} catch (Exception e) {
+            			em.getTransaction().rollback();
+            		} 
+        		}
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+		em.close();
+		System.out.println("Finish");
 		System.exit(0);
 	}
 }
